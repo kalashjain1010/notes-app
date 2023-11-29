4eefeb2592224118c5cdaed0b7b2d2ea4707b49e
kalash jain <kalashjain54@gmail.com>
2023-11-29 23:58:00 +0530

    creating models

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const path = require('path')
 const PORT = process.env.PORT || 3500
-const {logger} = require('./middleware/logger')
+const {logger, logEvents} = require('./middleware/logger')
 const errorHandler = require('./middleware/errorHandler')
 const cokkieParser = require('cookie-parser')
 const cors = require('cors')
 const corsOptions = require('./config/corsOptions')
+const connectDB = require('./config/dbConn')
+const mongoose = require('mongoose')
 
+
+connectDB()
 app.use(logger)
 app.use(cors(corsOptions))
 app.use(express.json())
@@ -27,4 +32,13 @@ app.all('*', (req, res) => {
 })
 app.use(errorHandler)
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB')
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+})
+
+mongoose.connection.on('error', err => {
+    console.log(err)
+    logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log')
+})
